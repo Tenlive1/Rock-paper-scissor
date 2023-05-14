9cbb317350d7808fa1df73913b62bc16874d476b
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-05-13 19:45:07 -0500

    finish with the assignment but later im pretty sure it will make me do a gui

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const pick = ["rock","paper","scissor"];
 function getcomputerchoice(){
-    return Math.floor(Math.random() * 3);
+    return pick[Math.floor(Math.random() * 3)];
 }
 
 function getplayerchoice(){
 
     let player = window.prompt("Please pick Rock, Paper or scissor").toLowerCase();
 
-    if(player == pick[0] || player == pick[1] || player ==  pick[3]){
+    if(player == pick[0] || player == pick[1] || player ==  pick[2]){
         return player;
     }
 
     while(true){
         let again = window.prompt("choice does not exist Please pick rock or paper or scissor").toLowerCase();
 
-        if(again == pick[0] || again == pick[1] || again ==  pick[3]){
+        if(again == pick[0] || again == pick[1] || again ==  pick[2]){
             return again
         }
     }
@@ -22,5 +22,25 @@ function getplayerchoice(){
     
 }
 
-console.log(getplayerchoice());
+function round(getplayerchoice,getcomputerchoice){
+    let player = getplayerchoice();
+    let computer = getcomputerchoice();
+
+    if (player == computer){
+        return "Draw";
+    }
+    else if(player == pick[0] && computer == pick[2] || player == pick[1] && computer == pick[0] || player == pick[2] && computer == pick[1]){
+        return "You Win! " + player + " beats " + computer;
+    }
+    else{
+        return "You Lose! " + player + " beats " + computer;
+    }
+
+}
+
+console.log(round(getplayerchoice,getcomputerchoice));
+console.log(round(getplayerchoice,getcomputerchoice));
+console.log(round(getplayerchoice,getcomputerchoice));
+console.log(round(getplayerchoice,getcomputerchoice));
+console.log(round(getplayerchoice,getcomputerchoice));
 
