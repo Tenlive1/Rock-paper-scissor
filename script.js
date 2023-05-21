2e7ea7db7674e2020abd51c0e144c83d9eb1a717
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-05-21 02:25:25 -0500

    finally able to load the page without refreshing

--- script.js
+++ script.js
@@ -13,11 +13,39 @@ function page(){
     const text = document.createElement('div');
     text.classList.add('texts');
 
-    const again = document.createElement("input");
-    again.type = "reset";
-    again.onclick = function(){document.location.reload(true);};
-    again.value = "PLAY AGAIN?"
+    const again = document.createElement("button");
+    again.classList.add("again");
     again.textContent = "PLAY AGAIN?";
+
+    again.addEventListener('click',() =>{
+        document.body.innerHTML = res;
+        const btns = document.querySelectorAll('.player-button');
+
+        btns.forEach((button) => {
+            
+
+            button.addEventListener('click', () => {
+                
+                let choice = button.textContent;
+                let computerchoice = getcomputerchoice();
+                
+                let test = round(choice.toLowerCase(),computerchoice)
+
+                if(test){
+                    const player = document.querySelector('.player-choice');
+                    const computer = document.querySelector('.computer-choice');
+                    player.textContent = choice;
+                    
+                    computer.textContent = computerchoice.toUpperCase();
+                }
+
+                
+
+            });
+            
+        });
+
+    });
 
     content.appendChild(text);
     content.appendChild(again);
@@ -34,44 +62,59 @@ function round(playerchoice,computerchoice){
     if (player == computer){
     }
     else if(player == pick[0] && computer == pick[2] || player == pick[1] && computer == pick[0] || player == pick[2] && computer == pick[1]){
-        playerscore.textContent = 5;
+        playerscore.textContent++;
     }
     else{ 
-        computerscore.textContent = 5;
+        computerscore.textContent++;
     }
 
-    if(playerscore.textContent == 5){
+    if(playerscore.textContent === "5"){
         page();
         const win = document.querySelector('.texts');
         win.textContent = "you win";
+        return false;
     }
-    else if(computerscore.textContent == 5){   
+    else if(computerscore.textContent === "5"){   
         page();
         const lose = document.querySelector('.texts');
         lose.textContent = "you lose";
+        return false;
     }
+    return true;
 
 }
 
 const btn = document.querySelectorAll('.player-button');
 
 btn.forEach((button) => {
+    
 
-    // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
         
         let choice = button.textContent;
         let computerchoice = getcomputerchoice();
-        round(choice.toLowerCase(),computerchoice);
+        
+        let test = round(choice.toLowerCase(),computerchoice)
 
-        const player = document.querySelector('.player-choice');
-        const computer = document.querySelector('.computer-choice');
-        player.textContent = choice;
-        computer.textContent = computerchoice.toUpperCase();
+        if(test){
+            const player = document.querySelector('.player-choice');
+            const computer = document.querySelector('.computer-choice');
+            player.textContent = choice;
+            
+            computer.textContent = computerchoice.toUpperCase();
+        }
 
+        
 
     });
      
   });
 
+
+  /*function load(){
+    var body = document.getElementsByTagName('body')[0];
+    var script = document.createElement('script');
+    script.src = "script.js";
+    body.appendChild(script);
+  }*/
 const res = document.body.innerHTML;
